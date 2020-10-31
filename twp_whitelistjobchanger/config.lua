3523beb54fe9ebfd08c7d105e646c9cf05e02999
Roids-Dev <73410725+Roids-Dev@users.noreply.github.com>
2020-10-31 07:47:02 -0500

    Added an example table to config.lua

--- twp_whitelistjobchanger/config.lua
+++ twp_whitelistjobchanger/config.lua
@@ -1,15 +1,25 @@
 Config = {}	
 
+----- EXAMPLE TABLE-----
+--EXAMPLE =
+--{
+--	"steam:id64hex",--"example 1"
+--	"steam:id64hex",--"example 2"	
+--}
+-----/EXAMPLE TABLE-----
 
 
+
+---------------------------------
 police =
 {
 	"steam:id64hex",--"example 1"
 	"steam:id64hex",--"example 2"	
 }
-
+---------------------------------
 doctor =
 {
 	"steam:id64hex"--"example 3"
 	
 }
+---------------------------------
