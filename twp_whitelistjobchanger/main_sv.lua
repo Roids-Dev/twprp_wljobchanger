40bf9b05e788c31ec2c6cd3e20b40524575ef5dd
Roids-Dev <54599495+itsroids@users.noreply.github.com>
2020-10-31 06:55:03 -0500

    Probably breaking the script lol
    Pretty much just trying to make the identifier in the function a variable so all job commands can use the same server event and function so it is easier for the enduser.

--- twp_whitelistjobchanger/main_sv.lua
+++ twp_whitelistjobchanger/main_sv.lua
@@ -2,8 +2,10 @@ VORP = exports.vorp_core:vorpAPI()
 
 
 -- Functions --
+-- If you download this and the script is broken just remove the below function and uncomment out the other 2 lol --
 local function checkCopWhitelist(id, job)
-    for key, value in pairs(police) do
+    local _job = job
+    for key, value in pairs(_job) do
         if id == value then
             return true
         end
@@ -11,14 +13,26 @@ local function checkCopWhitelist(id, job)
     return false
 end
 
-local function checkDocWhitelist(id, job)
-    for key, value in pairs(doctor) do
-        if id == value then
-            return true
-        end
-    end 
-    return false
-end
+
+
+
+--local function checkCopWhitelist(id, job)
+  --  for key, value in pairs(police) do
+    --    if id == value then
+      --      return true
+        --end
+    --end 
+    --return false
+--end
+
+--local function checkDocWhitelist(id, job)
+--    for key, value in pairs(doctor) do
+--        if id == value then
+--            return true
+--        end
+--   end 
+--    return false
+--end
 -- /Functions --
 
 
@@ -34,10 +48,10 @@ AddEventHandler('copdutycheck', function()
 local _source = source
 local user = VORP.getCharacter(_source)
 local _id = user.identifier
-if not checkCopWhitelist(_id) then
+if not checkCopWhitelist(_id, police) then
     print('whitelistnotworking')
 end
-if checkCopWhitelist(_id) then
+if checkCopWhitelist(_id, police) then
     print('whitelistworks')
     local _source = source
     VORP.setJob(_source, "police")
