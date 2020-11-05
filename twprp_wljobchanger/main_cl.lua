b90a0d8e918edf5d9ba94749c0f1c0ac80fc7c21
Roids-Dev <73410725+Roids-Dev@users.noreply.github.com>
2020-11-05 07:29:46 -0600

    Update main_cl.lua

--- twprp_wljobchanger/main_cl.lua
+++ twprp_wljobchanger/main_cl.lua
@@ -3,27 +3,35 @@
 ----- I update this frequently so check back often for the latest updates -----
 
 ----- https://github.com/Roids-Dev/twprp_whitelistjobchanger -----
-
+-------------------------------------------------
 -------------------------------------------------
 -- COPDUTY --
+-------------------------------------------------
 RegisterCommand('lawman', function()
     local job1 = police
     local job2 = 'police'
 	TriggerServerEvent('wlcheck', job1, job2)
 end)
+-------------------------------------------------
 -- /COPDUTY --
 -------------------------------------------------
+-------------------------------------------------
 -- DOCDUTY --
+-------------------------------------------------
 RegisterCommand('doctor', function()
     local job1 = doctor
     local job2 = 'doctor'
     TriggerServerEvent('wlcheck', job1, job2)
 end)
+-------------------------------------------------
 -- /DOCDUTY --
 -------------------------------------------------
+-------------------------------------------------
 -- OFFDUTY --
+-------------------------------------------------
 RegisterCommand('offduty', function()
     TriggerServerEvent('offduty')
 end)
+-------------------------------------------------
 -- /OFFDUTY --
 -------------------------------------------------
